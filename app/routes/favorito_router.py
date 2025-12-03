69ce08205702e74a487ee4ef7573b635b1c8adf0
Mauricio Bertuci <bertuci.mauricio@gmail.com>
2025-12-03 20:27:06 -0300

    teste

--- app/routes/favorito_router.py
+++ app/routes/favorito_router.py
@@ -5,6 +5,7 @@ from app.database import get_db
 from app.controllers.favorito_controller import *
 from fastapi.responses import RedirectResponse
 from app.auth import verificar_token 
+from app.models.usuario_model import UsuarioDB
 
 router = APIRouter(prefix="/favoritos")
 templates = Jinja2Templates(directory="app/views/templates")
@@ -18,17 +19,32 @@ def list_favorites(request: Request, db: Session = Depends(get_db)):
     payload = verificar_token(token)
     if not payload:
         return RedirectResponse(url="/login", status_code=303)
-    id_usuario = payload.get("id")
 
+    # pega id do usuário do token
+    id_usuario = payload.get("id") or payload.get("id_usuario")
+
+    # busca usuário no banco
+    usuario = (
+        db.query(UsuarioDB)
+        .filter(UsuarioDB.id_cliente == id_usuario)
+        .first()
+    )
+
+    if not usuario:
+        return RedirectResponse(url="/login", status_code=303)
+
+    # busca favoritos (produtos)
     favoritos = listar_favoritos(id_usuario, db)
 
     return templates.TemplateResponse(
         "favoritos.html",
         {
             "request": request,
-            "favoritos": favoritos,
+            "favoritos": favoritos,  
+            "usuario": usuario,
         },
     )
+
 
 @router.post("/adicionar/{id_produto}")
 def add_favorite(id_produto: int, request: Request, db: Session = Depends(get_db)):
