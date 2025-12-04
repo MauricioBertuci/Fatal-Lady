e7f3ce7810d01a6b5a5b18e51fec072b9c82d706
Gabrielly Macario <gabriellymacario872@gmail.com>
2025-12-04 16:39:50 -0300

    login google

--- app/oauth_config.py
+++ app/oauth_config.py
@@ -1,4 +1,3 @@
-
 from authlib.integrations.starlette_client import OAuth
 from starlette.config import Config
 from dotenv import load_dotenv
@@ -7,26 +6,28 @@ import os
 BASE_DIR = os.path.dirname(__file__)
 ENV_PATH = os.path.join(BASE_DIR, ".env")
 
-load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
+# Carrega variáveis do .env
+load_dotenv(ENV_PATH)
 
-config = Config(os.path.join(os.path.dirname(__file__), ".env"))
+config = Config(ENV_PATH)
 oauth = OAuth(config)
 
-#  Login com Google (OAuth2 / OpenID Connect)
+# Login com Google (OAuth2 / OpenID Connect)
 oauth.register(
     name="google",
-    server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
     client_id=os.getenv("GOOGLE_CLIENT_ID") or config("GOOGLE_CLIENT_ID", default=None),
     client_secret=os.getenv("GOOGLE_CLIENT_SECRET") or config("GOOGLE_CLIENT_SECRET", default=None),
+    server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
     client_kwargs={"scope": "openid email profile"},
 )
 
-facebook = oauth.register(
-    name='facebook',
+# Login com Facebook (mantive sua config)
+oauth.register(
+    name="facebook",
     client_id=os.getenv("FACEBOOK_CLIENT_ID"),
     client_secret=os.getenv("FACEBOOK_CLIENT_SECRET"),
-    access_token_url='https://graph.facebook.com/v21.0/oauth/access_token',  # ← v21
-    authorize_url='https://www.facebook.com/v21.0/dialog/oauth',  # ← v21
-    api_base_url='https://graph.facebook.com/v21.0/',  # ← v21
+    access_token_url='https://graph.facebook.com/v21.0/oauth/access_token',
+    authorize_url='https://www.facebook.com/v21.0/dialog/oauth',
+    api_base_url='https://graph.facebook.com/v21.0/',
     client_kwargs={'scope': 'email public_profile'}
 )
